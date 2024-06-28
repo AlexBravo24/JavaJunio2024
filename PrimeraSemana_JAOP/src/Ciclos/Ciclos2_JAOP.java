793d9019c188a11a353136f5746b5a7f8561f4a8
chealberto <chealberto.op@hotmail.com>
2024-06-28 11:50:52 -0600

    Ejercicios semana 1 JAOP

---
+++ PrimeraSemana_JAOP/src/Ciclos/Ciclos2_JAOP.java
@@ -0,0 +1,12 @@
+package Ciclos;
+
+public class Ciclos2_JAOP {
+
+	public static void main(String[] args) {
+		// 2.- Programa un algoritmo que realice la tabla de multiplicar de un numero 
+		//introducido desde teclado, hasta la iteración deseada por el usuario. 
+		//Ejemplo, tabla de 23 hasta el 95. 23 x 95 = ???
+
+	}
+
+}
---
+++ PrimeraSemana_JAOP/src/com/entradaDeDatosJava.java
@@ -0,0 +1,55 @@
+package com;
+
+import java.util.Scanner;
+
+public class entradaDeDatosJava {
+
+	public static void main(String[] args) {
+		// vamos a aprender introducir valores que pueden ser leidos desde el teclado
+        //Ejemplo guardar unn valor numerico
+		//declaramos primero una variable donde guardar ese valor
+		
+		/*int edad;
+		
+		//Creamos una variable de una clase llamada scanner
+		
+		Scanner entrada = new Scanner (System.in);
+				
+				System.out.println("Introduce tu edad:");
+		        edad=entrada.nextInt();
+		        
+		        System.out.println("Introduce tu nombre:");
+		        String nombre = entrada.next();		        	        
+		        		       
+		        //Una vez guardado el valor podemos hacer uso de el para cualquier operacion
+		        // Si queremos ahorrar memoria y que el scanner no quede abierto y ya no puedan ingresar mas datos
+		        
+		        entrada.close();
+		        
+		       System.out.println("El siguiente año tendras: " + (nombre) + (edad+1) + ("años"));*/
+		
+		
+		//OPERADOR MODULO %- Ayuda a devolver el valor del residuo de una division
+		
+		int x = 10;
+		
+		int resultado = 10%3;
+		
+		System.out.println(resultado);
+		
+		
+		       
+		       
+		       
+		       
+		       
+		       
+		       
+		       
+		       
+		       
+		        
+			
+	}
+
+}
